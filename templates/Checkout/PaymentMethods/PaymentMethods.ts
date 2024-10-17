e7e400b0bf5f03cc245e5cde8e8c7b3d55d9e836
Shaheer <shaheer@iversion.com.au>
2024-10-17 18:11:10 +0500

    Added the dynamic component to Main paymentMethod template from Stripe

--- templates/Checkout/PaymentMethods/PaymentMethods.ts
+++ templates/Checkout/PaymentMethods/PaymentMethods.ts
@@ -3,6 +3,9 @@ import { useCheckoutStore } from "../../../stores/Checkout";
 export default defineComponent({
   name: 'PaymentMethodScript',  
   setup() {	
+let stripe;
+let elements;
+	const checkoutStore = useCheckoutStore()
   	const Stripe = defineAsyncComponent(() =>
 			import('@/components/template_01/Checkout/PaymentMethods/Stripe.vue')
 		)
@@ -33,11 +36,95 @@ export default defineComponent({
 		    }
 		}
 
+		const getpaymentMethod = async (paymentMethod,inputData,totalPrice) => {
+			try {
+				inputData.paymentmethod = paymentMethod
+				const newPrice = parseFloat(totalPrice.total)
+				if (paymentMethod == 'web-stripe' && stripe !== null) {
+					document.querySelector('#card-element').innerHTML = (`<div class="payment-loading text-center"><svg xmlns="http://www.w3.org/2000/svg" width="5rem" height="5rem" viewBox="0 0 24 24"><circle cx="12" cy="12" r="0" fill="#f68c38"><animate id="svgSpinnersPulse30" fill="freeze" attributeName="r" begin="0;svgSpinnersPulse32.begin+0.8s" calcMode="spline" dur="2.4s" keySplines=".52,.6,.25,.99" values="0;11"/><animate fill="freeze" attributeName="opacity" begin="0;svgSpinnersPulse32.begin+0.8s" calcMode="spline" dur="2.4s" keySplines=".52,.6,.25,.99" values="1;0"/></circle><circle cx="12" cy="12" r="0" fill="#f68c38"><animate id="svgSpinnersPulse31" fill="freeze" attributeName="r" begin="svgSpinnersPulse30.begin+0.8s" calcMode="spline" dur="2.4s" keySplines=".52,.6,.25,.99" values="0;11"/><animate fill="freeze" attributeName="opacity" begin="svgSpinnersPulse30.begin+0.8s" calcMode="spline" dur="2.4s" keySplines=".52,.6,.25,.99" values="1;0"/></circle><circle cx="12" cy="12" r="0" fill="#f68c38"><animate id="svgSpinnersPulse32" fill="freeze" attributeName="r" begin="svgSpinnersPulse30.begin+1.6s" calcMode="spline" dur="2.4s" keySplines=".52,.6,.25,.99" values="0;11"/><animate fill="freeze" attributeName="opacity" begin="svgSpinnersPulse30.begin+1.6s" calcMode="spline" dur="2.4s" keySplines=".52,.6,.25,.99" values="1;0"/></circle></svg></div>`);
+					const appearance = {
+						theme: 'stripe'
+					}
+					await checkoutStore.paymentMethods({
+						clientsceret: checkoutStore.publishableKey.clientsceret,
+						paymentmethod: paymentMethod,
+						total: newPrice * 100
+					})
+					const clientSecret = checkoutStore.publishableKey.clientsceret
+					stripe = await loadStripe(checkoutStore.publishableKey.key)
+					elements = stripe.elements({ appearance, clientSecret, loader })
+					// const linkAuthenticationElement = elements.create("linkAuthentication");
+	
+					const paymentElementOptions = {
+						layout: {
+							type: 'tabs',
+						}
+					}
+					// Mount the Elements to their corresponding DOM node
+					const cardElement = elements.create('payment', paymentElementOptions)
+					cardElement.mount('#card-element')
+				}
+				if (paymentMethod == 'web-zippay' || paymentMethod == 'web-direct-deposit' || paymentMethod == 'web-paypal' || paymentMethod == 'web-eway') {
+					await checkoutStore.paymentMethods({
+						paymentmethod: paymentMethod,
+						total: newPrice
+					})
+					if (paymentMethod == 'web-eway') {
+						inputData.ewayKey = checkoutStore.publishableKey.key
+					}
+				}
+	
+				if (paymentMethod == 'web-till-payment') {
+					await checkoutStore.paymentMethods({
+						paymentmethod: paymentMethod,
+						total: newPrice
+					})
+	
+					payment.init('TFtrYq0SAcyy5l2cxi3a', 'number_div', 'cvv_div', function (payment) {
+						payment.setNumberStyle({
+							'width': '100%',
+							'height': '100%',
+							'border': 'none',
+							'input:focus-visible': {
+								'outline': '-webkit-focus-ring-color auto 0px'
+							}
+						});
+						payment.setCvvStyle({
+							'width': '100%',
+							'height': '100%',
+							'border': 'none'
+						});
+						payment.numberOn('input', function () {
+							var data = {
+								card_holder: inputData.till_cardname,
+								month: inputData.till_expirymonth,
+								year: inputData.till_expiryyear,
+							};
+							payment.tokenize(data, //additional data, MUST include card_holder (or first_name & last_name), month and year
+								function (token) { //success callback function
+									inputData.clientsceret = token
+								},
+								function (errors) { //error callback function
+									console.log(errors)
+									//render error information here
+								}
+							)
+						})
+					});
+	
+				}
+				checkoutStore.saveToCheckoutSession(inputData)
+			} catch (error) {
+				console.log('Error', error)
+			}
+		}
+
 	return{
 		Stripe,
 		Eway,
 		Till,
 		Paypal,
+		getpaymentMethod,
 		getPaymentComponent
 	}
 
